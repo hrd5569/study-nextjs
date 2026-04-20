ef72a8738964e3f46d209870a60d36e6b2a94281
原田光 <haradahikari@HRDMacBook-Air.local>
2026-04-20 14:44:32 +0900

    Chapter 15: Adding Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
+import type { Metadata } from 'next';
 import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Learn Dashboard, built with App Router.',
+  metadataBase: new URL('http://localhost:3000'),
+};
 
 export default function RootLayout({
   children,
@@ -7,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
